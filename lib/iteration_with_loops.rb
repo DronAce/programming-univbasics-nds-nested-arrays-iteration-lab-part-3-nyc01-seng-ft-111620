7848b57548ebcc393bc7ab7720bfd09f5b950b51
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-30 18:13:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,11 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  srcg = []
   for i in 0...src.size
     for y in 0...src[i].size
       if src[i][y].instance_of?
-        src << src[i][y]
+        srcg << src[i][y]
       end
     end
   end
